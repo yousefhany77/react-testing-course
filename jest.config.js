b12373c25a03c3e5a665f089e9e78dfdf686c5c4
yousefhany77 <yousefhany2014@gmail.com>
2022-10-11 21:37:22 +0200

    coverage

--- jest.config.js
+++ jest.config.js
@@ -19,6 +19,14 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: -10,
+    },
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
