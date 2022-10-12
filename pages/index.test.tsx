4d6400754f3f735dc026b31449bde802fcf2de68
yousefhany77 <yousefhany2014@gmail.com>
2022-10-12 21:46:31 +0200

    added tests

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,9 +1,43 @@
-import { render, screen } from "@testing-library/react";
-import Home from ".";
-import "@testing-library/jest-dom";
-it("renders a heading", () => {
-  render(<Home />);
-  const myComponent = screen.getByText("Hello");
-  expect(myComponent).toBeInTheDocument();
+import { screen, render } from "@testing-library/react";
+import Index from ".";
+
+describe("Index", () => {
+  it("Render correctly", () => {
+    render(<Index />);
+
+    const pageHeading = screen.getByRole("heading", {
+      name: /job application form/i,
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+    const input = screen.getByRole("textbox", { name: /name/i });
+    expect(input).toBeInTheDocument();
+
+    const comboBox = screen.getByRole("combobox");
+    expect(comboBox).toBeInTheDocument();
+
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox).toBeInTheDocument();
+
+    const sumbitButton = screen.getByRole("button", { name: /sumbit/i });
+    expect(sumbitButton).toBeInTheDocument();
+  });
+  it("should Render List items in Order", () => {
+    render(<Index />);
+    const listItems = screen.getAllByRole("listitem");
+    const skills = ["html", "javascript", "React"];
+
+    expect(listItems).toHaveLength(3);
+    expect(listItems.map((item) => item.textContent)).toEqual(skills);
+  });
+  it("should render delayed button", async () => {
+    render(<Index />);
+
+    const dylayed = await screen.findByRole(
+      "button",
+      { name: "delay" },
+      { timeout: 2000 }
+    );
+    expect(dylayed).toBeInTheDocument();
+  });
 });
- 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,56 @@
-import React, { FunctionComponent } from "react";
-const Home: FunctionComponent = () => (
-  <div>
-   Hello
-  </div>
-);
+import React, { FunctionComponent, useEffect, useState } from "react";
+const skills = ["html", "javascript", "React"];
+const Home: FunctionComponent = () => {
+  const [state, setstate] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setstate(true);
+    }, 1005);
+  });
+  return (
+    <div
+      style={
+        {
+          display: "flex",
+          "flex-direction": "column",
+          "text-align": "center",
+          padding: "5rem",
+          gap: "1rem",
+        } as React.CSSProperties
+      }
+    >
+      <h1>Job Application Form</h1>
+      <h2>Section 1</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div>
+          <label htmlFor="job-location">Jop Location</label>
+          <select id="job-location">
+            <option value="">Select a Country</option>
+            <option value="EG">EG</option>
+            <option value="USA">USA</option>
+            <option value="UK">UK</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            i agree
+            <input type="checkbox" id="terms" />
+          </label>
+        </div>
+      </form>
+      <button>Sumbit</button>
+      {state && <button>delay</button>}
+      <ul>
+        {skills.map((sk) => (
+          <li key={sk}>{sk}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Home;
